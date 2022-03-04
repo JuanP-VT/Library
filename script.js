4a1c915aac26b073b548b36798070709f3b2a7f5
PB-vt <cv.juanp@gmail.com>
2022-03-03 17:20:15 -0700

    Script now can emmbed the bookcover img

--- script.js
+++ script.js
@@ -12,30 +12,43 @@ function addBook(ev){
         name: document.getElementById('name').value,
         author: document.getElementById('author').value,
         pages: document.getElementById('pages').value,
-        read : false
+        url: document.getElementById('imgurl').value,
+        read : false,
     }
+
+    //Create bookcard
     const bookcard = document.createElement('div');
     bookcard.classList.add('bookcard')
-    bookcard.id = book.name
+    bookcard.id = book.id
     const bookcardheader = document.createElement('div');
     bookcardheader.classList.add('bookcardheader')
+
+
     const iconSee = document.createElement('img')
     iconSee.src = "./icons/eye.png"
     iconSee.classList.add('icon')
+
+    
     const iconDelete = document.createElement('img')
     //Inserts icon with event to remove this div and object
     iconDelete.src ='./icons/delete.png'
     iconDelete.addEventListener('click',() =>{
-        const el = document.getElementById(book.name)
-        const removeIndex = myLibrary.map(item => item.name).indexOf(book.name);
+        const el = document.getElementById(book.id)
+        const removeIndex = myLibrary.map(item => item.id).indexOf(book.id);
         ~removeIndex && myLibrary.splice(removeIndex, 1);
         el.remove();
     })
     iconDelete.classList.add('icon')
     bookcardheader.appendChild(iconSee)
     bookcardheader.appendChild(iconDelete)
+
+    //Adding cover if one is submitted
     const bookcardimg = document.createElement('div');
     bookcardimg.classList.add('bookcardimg')
+    const cover = document.createElement('img')
+    cover.src = book.url;
+    bookcardimg.appendChild(cover);
+
     const bookcardfooter = document.createElement('div');
     bookcardfooter.classList.add('bookcardfooter')
     const bookname = document.createElement('p')
@@ -57,6 +70,7 @@ function addBook(ev){
     bookcard.appendChild(bookcardfooter)
     const library = document.querySelector('#library')
     library.appendChild(bookcard)
+    //Save object
     myLibrary.push(book);
     document.forms[0].reset();
 }
