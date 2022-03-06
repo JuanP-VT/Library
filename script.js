9ec25aa43e60bacabe2a40b1fcae92edf6dcfac6
PB-vt <cv.juanp@gmail.com>
2022-03-05 17:13:54 -0700

    Objects are now displayed in the screen with a array loop

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-
+const library = document.querySelector('#library')
 document.addEventListener('DOMContentLoaded',() =>{
     document.getElementById('btn').addEventListener('click',addBook)
 });
@@ -13,81 +13,101 @@ function addBook(ev){
         author: document.getElementById('author').value,
         pages: document.getElementById('pages').value,
         url: document.getElementById('imgurl').value,
-        read : false,
+        read : 'Not Read',
     }
-
-    //Create bookcard
-    const bookcard = document.createElement('div');
-    bookcard.classList.add('bookcard')
-    bookcard.id = book.id
-    const bookcardheader = document.createElement('div');
-    bookcardheader.classList.add('bookcardheader')
-
-
-    const iconSee = document.createElement('img')
-    iconSee.src = "./icons/eye.png"
-    iconSee.classList.add('icon')
-    const currentBooks = myLibrary.length
-    iconSee.addEventListener('click',() =>{
-        console.log(currentBooks)
-        if (myLibrary[currentBooks].read == true){
-            myLibrary[currentBooks].read = false
-        }
-        else if (myLibrary[currentBooks].read == false){
-            myLibrary[currentBooks].read = true
-        }
-
-    })
-    const iconDelete = document.createElement('img')
-    //Inserts icon with event to remove this div and object
-    iconDelete.src ='./icons/delete.png'
-    iconDelete.addEventListener('click',() =>{
-        const el = document.getElementById(book.name)
-        const removeIndex = myLibrary.map(item => item.id).indexOf(book.name);
-        ~removeIndex && myLibrary.splice(removeIndex, 1);
-        el.remove();
-    })
-    iconDelete.classList.add('icon')
-    bookcardheader.appendChild(iconSee)
-    bookcardheader.appendChild(iconDelete)
-
-    //Adding cover if one is submitted
-    const bookcardimg = document.createElement('div');
-    bookcardimg.classList.add('bookcardimg')
-    const cover = document.createElement('img')
-    cover.src = book.url;
-    bookcardimg.appendChild(cover);
-
-    const bookcardfooter = document.createElement('div');
-    bookcardfooter.classList.add('bookcardfooter')
-    const bookname = document.createElement('p')
-    bookname.classList.add('bookname')
-    bookname.textContent = book.name;
-    const bookauthor = document.createElement('p')
-    bookauthor.classList.add('bookauthor')
-    bookauthor.textContent = book.author;
-    const bookpages = document.createElement('p')
-    bookpages.textContent = book.pages;
-    const bookread = document.createElement('p')
-    bookread.textContent ='Not read'
-    bookcardfooter.appendChild(bookname)
-    bookcardfooter.appendChild(bookauthor)
-    bookcardfooter.appendChild(bookpages)
-    bookcardfooter.appendChild(bookread)
-    bookcard.appendChild(bookcardheader)
-    bookcard.appendChild(bookcardimg)
-    bookcard.appendChild(bookcardfooter)
-    const library = document.querySelector('#library')
-    library.appendChild(bookcard)
-    //Save object
     myLibrary.push(book);
     document.forms[0].reset();
+    runArray()
 }
 
+function runArray(){
+    reset()
+myLibrary.forEach(element => {
+//Creates bookcard
+console.log(myLibrary)
+//Bookcard header
+    const BookCard = document.createElement('div')
+    BookCard.classList.add('bookcard')
+    const BookCardHeader = document.createElement('div')
+    BookCardHeader.classList.add('bookcardheader')
+    const iconEye = document.createElement('img')
+    iconEye.src = ('./icons/eye.png')
+    iconEye.classList.add('icon')
+    iconEye.addEventListener('click', () => {
+        if (element.read === 'Not Read'){
+            element.read = 'Read'
+            BookIsRead.textContent = element.read
+        }
+        else if (element.read === 'Read'){
+            element.read ='Not Read'
+            BookIsRead.textContent = element.read
+
+        }
+
+    })
+    const deleteIcon = document.createElement('img')
+    deleteIcon.src = './icons/delete.png'
+    deleteIcon.classList.add('icon')
+    deleteIcon.addEventListener('click',() =>{
+        const targetBook = element.name
+        const bookToDelete = myLibrary.findIndex(em => em.name === targetBook)
+        myLibrary.splice(bookToDelete,1)
+        runArray()
+
+    })
+
+//Cardimg
+    const BookCardImg = document.createElement('div')
+    BookCardImg.classList.add('bookcardimg')
+    const BookCover = document.createElement('img')
+    BookCover.src = (element.url)
+
+    //Bookcard footer
+    const BookCardFooter = document.createElement('div')
+    BookCardFooter.classList.add('bookcardfooter')
+    
+    const BookName = document.createElement('p')
+    BookName.textContent = element.name
+    const BookAuthor = document.createElement('p')
+    BookAuthor.textContent = element.author
+    const BookPages = document.createElement('p')
+    BookPages.textContent = element.pages
+    const BookIsRead = document.createElement('p')
+    BookIsRead.textContent = element.read
+    BookIsRead.setAttribute
+
+
+
+    BookCard.appendChild(BookCardHeader)
+    BookCard.appendChild(BookCardImg)
+    BookCard.appendChild(BookCardFooter)
+
+
+
+    BookCardHeader.appendChild(deleteIcon)
+    BookCardHeader.appendChild(iconEye)
+
+    BookCardImg.appendChild(BookCover)
+
+    BookCardFooter.appendChild(BookName)
+    BookCardFooter.appendChild(BookAuthor)
+    BookCardFooter.appendChild(BookPages)
+    BookCardFooter.appendChild(BookIsRead)
 
 
 
 
+    const library = document.querySelector('#library')
+    library.appendChild(BookCard)
+});
+}
 
+function reset(){
+    while(library.firstChild){
+        library.removeChild(library.firstChild)
+    }
+}
 
-
+function addIcons(){
+    document.querySelectorAll
+}
